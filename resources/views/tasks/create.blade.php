d6ee3ffcff19d289534068fc38e94631fdcaa809
hayato.yoshida <ec2-user@ip-172-31-16-129.us-east-2.compute.internal>
2018-05-30 01:19:34 +0000

    CRUD tasks

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>タスクリスト新規作成ページ</h1>
+
+ @if (count($errors) > 0)
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+
+@endsection
+
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+
+    <p>タイトル: {{ $task->title }}</p>
+    <p>メッセージ: {{ $task->content }}</p>
+
+    {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
+
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
