b82988ae680f24aee95c12b63106a64d765253b0
hayato.yoshida <ec2-user@ip-172-31-16-129.us-east-2.compute.internal>
2018-06-05 04:18:25 +0000

    'add bootstrap'

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,18 +2,25 @@
 
 @section('content')
 
+ <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6 ">
+
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
+	<div class='form-group'>
         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status') !!}
+	</div>
 
+	<div class='form-group'>
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
+	</div>
 
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
-
+	</div>
+ </div>
 @endsection
